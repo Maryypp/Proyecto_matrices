542e4f63c4b568a23c6430e4b18b23dc1f788fb5
Maryypp <marypvh26@gmail.com>
2024-09-10 10:57:39 -0600

    1

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+class Examen  {
+
+    val plantillas= CharArray(12)
+    val nombres = Array(4){""}
+    val notas: FloatArray = FloatArray(4)
+    val respuestas: CharArray = charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d')
+    var contador: Int = 0
+
+    fun constructor(){
+       var nombres = arrayOf("Marta")
+
+
+    }
+
+     }
+
+
+
